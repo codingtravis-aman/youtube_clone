30e4142237878a4e5bde2c2d9856dbb7f1240fc8
satish jha <satishjha844@gmail.com>
2023-05-30 03:41:14 -0700

    Your commit message

---
+++ src/context/contextApi.js
@@ -0,0 +1,44 @@
+import React, { createContext, useState, useEffect } from "react";
+import { fetchDataFromApi } from "../utils/api";
+
+export const Context = createContext();
+
+export const AppContext = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [searchResults, setSearchResults] = useState(null); // Update initial state to null
+  const [selectCategories, setSelectCategories] = useState("New");
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  useEffect(() => {
+    fetchSelectedCategoryData(selectCategories);
+  }, [selectCategories]);
+
+  const fetchSelectedCategoryData = (query) => {
+    setLoading(true);
+    fetchDataFromApi(`search/?q=${query}`).then(({ contents }) => {
+      console.log(contents);
+      setSearchResults(contents);
+      setLoading(false);
+    }).catch((error) => {
+      console.error("Error fetching data:", error);
+      setLoading(false);
+    });
+  };
+
+  return (
+    <Context.Provider
+      value={{
+        loading,
+        setLoading,
+        searchResults,
+        setSearchResults,
+        selectCategories,
+        setSelectCategories,
+        mobileMenu,
+        setMobileMenu,
+      }}
+    >
+      {props.children}
+    </Context.Provider>
+  );
+};
